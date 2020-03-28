3e3579cdf771ef4331bc71f4a80b0222769eed13
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 00:51:24 +0100

    Update install.sh

--- theme_numix-paper-icon/install.sh
+++ theme_numix-paper-icon/install.sh
@@ -8,9 +8,9 @@ icon_default="Numix-Paper"
 
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
 apt-get install -y numix-icon-theme
-dpkg -i "$base_dir"/paper-icon-theme*.deb
 (dd if="$base_dir"/paper-icon-theme*.aa" bs=1M; dd if="$base_dir"/paper-iscon-theme*.bb" bs=1M) > /tmp/paper-icon-theme.deb
-dpkg -i /tmp/bunsen-paper-icon-theme.deb
+dpkg -i /tmp/paper-icon-theme.deb
+dpkg -i "$base_dir"/bunsen-paper-icon-theme*.deb
 
 if [ ! -d /usr/share/icons/Numix/ ]; then
 	echo "$(basename $0) ERROR: Numix theme is not installed"
