4cf9b43afdc2b1a5736eaae5befa84d213b70393
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 23:38:55 +0200

    Create install.sh

---
+++ script_powermenu/install.sh
@@ -0,0 +1,8 @@
+# ACTION: Install exit menu script based in rofi
+# DEFAULT: y
+
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+base_dir="$(dirname "$(readlink -f "$0")")"
+
