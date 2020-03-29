282f7d552a43f7cfbdae5c708373156fa826691d
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 03:23:50 +0200

    Update and rename theme_tint2.sh to install.sh

--- install_tint2/install.sh
+++ install_tint2/install.sh
@@ -12,11 +12,20 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 # Check if virtualmachine:
 cat /proc/cpuinfo | grep -i hypervisor &>/dev/null && virtualmachine="true"
 
-for d in /usr/share/bunsen/skel/.config/tint2/  /home/*/.config/tint2/; do
-	cp -v "$base_dir/"*tint* "$d"
+for d in /etc/skel/  /home/*/; do
+    # Skip dirs in /home that not are user home
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+	
+	# Create config folders if no exists
+	d="$d/.config/"; [ ! -d "$d" ] && { mkdir -v "$d"; chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"; }
+	d="$d/tint2/";  [ ! -d "$d" ] && { mkdir -v "$d"; chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"; }
+	
+	cp -v "$base_dir/"*tint* "$d" && chown $(stat "$(dirname "$d")" -c %u:%g) "$d"/*tint*;
+	
 	[ "$laptop" ] && [ ! "$virtualmachine" ] && tint_version="_laptop"
 	
 	# Set taskbar.tint and menu.tint as default tints
 	echo "$d/taskbar${tint_version}.tint" > "$d/tint2-sessionfile"
 	echo "$d/menu.tint" >> "$d/tint2-sessionfile"
+	chown $(stat "$(dirname "$d")" -c %u:%g) "$d/tint2-sessionfile"; 
 done
