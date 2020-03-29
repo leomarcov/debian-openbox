eaa8edde7f85699d85de8fd6c6599ee0a2c09648
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 18:23:47 +0200

    Add files via upload

---
+++ install_virtualbox/install_virtualbox.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# ACTION: Install VirtualBox 6.1 and Extension Pack, add to repositories and insert to Openbox menu
+# DEFAULT: y
+
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+
+vb_package="virtualbox-6.1"
+ep_url="https://download.virtualbox.org/virtualbox/6.1.4/Oracle_VM_VirtualBox_Extension_Pack-6.1.4.vbox-extpack"
+main_distro="$(cat /etc/apt/sources.list | grep ^deb | awk '{print $3}' | head -1)"
+
+# Install repositories and update
+if ! grep -R "download.virtualbox.org" /etc/apt/ &> /dev/null; then
+	echo "deb http://download.virtualbox.org/virtualbox/debian $main_distro contrib" > /etc/apt/sources.list.d/virtualbox.list
+	wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+	wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
+	apt-get update	
+fi
+
+# Install packages
+apt-get install -y linux-headers-$(uname -r) "$vb_package" || exit 1
+
+# Add VirtualBox in OpenBox menu:
+for f in /usr/share/bunsen/skel/.config/openbox/menu.xml  /home/*/.config/openbox/menu.xml ; do
+	! grep -q '<command>virtualbox<\/command>' "$f" && sed -i '0,/<separator\/>/s//<item label="VirtualBox"><action name="Execute"><command>virtualbox<\/command><\/action><\/item> <separator\/>/' "$f"
+done
+
+
+# Get Extension Pack
+if ! which vboxmanage &> /dev/null; then
+  echo "VirtualBox is not installed"
+  exit 1
+fi
+
+t=$(mktemp -d)
+wget -P "$t" "$ep_url"  
+[ $? -eq 0 ] && yes | vboxmanage extpack install --replace "$t"/*extpack 
+rm -rf "$t"
