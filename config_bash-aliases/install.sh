7eaf64dbfe3ca42d336e01097e161603ce0e1606
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 01:23:47 +0100

    Update install.sh

--- config_bash-aliases/install.sh
+++ config_bash-aliases/install.sh
@@ -6,9 +6,12 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 base_dir="$(dirname "$(readlink -f "$0")")"
-comment_mark="#BL-POSTINSTALL"
+comment_mark="#DEBIAN-OPENBOX-SCRIPT"
 
-for d in /home/*  /usr/share/bunsen/skel/  /root; do
+for d in  /home/*/  /usr/etc/skel/  /root; do
+	# Skip dirs in /home that not are user home
+	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+    
 	sed -i "/$comment_mark/Id" "$d/.bash_aliases" 2> /dev/null
 	cat "$base_dir/bash_aliases" >> "$d/.bash_aliases"
 done
