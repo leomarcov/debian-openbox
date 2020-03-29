b6105c79184ae0c093b4b84126a846782bb208dd
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-30 01:06:37 +0200

    Update and rename install_some-packages/install.sh to 00_install_basic-packages/install.sh

--- 00_install_basic-packages/install.sh
+++ 00_install_basic-packages/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ACTION: Install some useful packages
+# ACTION: Install some basic packages
 # INFO: Useful packages: vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar gmtp mtp-tools mailutils traceroute acl netcat
 # DEFAULT: y
 
