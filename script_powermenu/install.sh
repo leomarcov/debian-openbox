69bc89156d2b573573b3d819ffe8dddbf80d59e0
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-30 00:22:32 +0200

    Update install.sh

--- script_powermenu/install.sh
+++ script_powermenu/install.sh
@@ -8,3 +8,17 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 
 cp -v "$base_dir/powermenu" /usr/bin
 chmod +x /usr/bin/powermenu
+
+for d in /etc/skel/  /home/*/; do
+    # Skip dirs in /home that not are user home
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+
+	# Create config folders if no exists
+	d2="$d"
+	d="$d/.config/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
+	d="$d/rofi/";  [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
+
+	# Copy theme
+	f="box.rasi"
+	cp -v "$base_dir/$f" "$d/" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d/$f"
+done
