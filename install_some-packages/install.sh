1ebbba428e03d60bf19e65ab4846be5742370adf
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 01:45:29 +0100

    Update and rename install_some-packages.sh to install.sh

--- install_some-packages/install.sh
+++ install_some-packages/install.sh
@@ -7,6 +7,5 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
-apt-get install -y vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar gmtp mtp-tools mailutils traceroute acl
-# apt-get install -y haveged  # Avoid delay first login in Helium
+apt-get install -y vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar zip unzip gmtp mtp-tools mailutils traceroute acl
   
