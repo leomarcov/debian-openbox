dcbd6a1f3bcef9f9e814cb4f7ed6fddaa16859da
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 21:11:16 +0200

    Add files via upload

---
+++ script_autopoweroff/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ACTION: Install script poweroff_last for automatize shutdown if no users logged in 20 minutes
+# DEFAULT: y
+
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+base_dir="$(dirname "$(readlink -f "$0")")"
+bash "$base_dir/autopoweroff" -I 20
