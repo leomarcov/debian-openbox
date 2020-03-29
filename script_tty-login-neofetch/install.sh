0a8aef6da6b5ef2176baa6457a8cf6c755b11c37
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 04:07:56 +0200

    Update install.sh

--- script_tty-login-neofetch/install.sh
+++ script_tty-login-neofetch/install.sh
@@ -6,7 +6,7 @@
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
-comment_mark="#bl-postinstall.sh"
+comment_mark="#DEBIAN-OPENGOX"
 base_dir="$(dirname "$(readlink -f "$0")")"
 
 # Config runlevel 3
@@ -16,9 +16,6 @@ systemctl set-default multi-user.target
 # Install physlock
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
 apt-get -y install physlock
-	
-# Config physlock as default locker
-sed  -i 's/^LOCK_COMMAND *= *.*/LOCK_COMMAND=\(\/usr\/bin\/physlock\)/' /usr/bin/bl-lock
 	
 # Config physlock for start after suspend
 cp "$base_dir"/physlock.service /etc/systemd/system/
@@ -47,7 +44,3 @@ fi
 # Remove light-locker
 apt-get -y remove light-locker
 
-# Solve tty error
-for f in /etc/skel/.bash_logout /home/*/.bash_logout; do
-	sed -i 's/clear_console/clear/g' "$f"
-done
