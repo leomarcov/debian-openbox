dc6c9c9dd97219172c8a2c7fcb551d0b1981bb70
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 18:31:50 +0200

    Add files via upload

---
+++ theme_notify/theme_notify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ACTION: Install clear xfce4-notify theme
+# DEFAULT: y
+
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+base_dir="$(dirname "$(readlink -f "$0")")"
+
+# Create theme:
+mkdir -p "/usr/share/themes/clear-notify/xfce-notify-4.0/"
+cp -v "$base_dir/clear_xfce-notify-4.0_gtk.css" "/usr/share/themes/clear-notify/xfce-notify-4.0/gtk.css"
+
+for f in  /usr/share/bunsen/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-notifyd.xml  /home/*/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-notifyd.xml ; do
+  sed -i '/name="theme"/s/value=".*"/value="clear-notify"/' "$f"
+done
+
