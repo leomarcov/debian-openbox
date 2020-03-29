afa674beb0b7a2604ae65dbb550798bdc9b3250f
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 15:26:23 +0200

    Update install.sh

--- install_volume-control/install.sh
+++ install_volume-control/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# ACTION: Install Openbox and tools for full environment
-# INFO: GoHome modified is a clear and cool Openbox theme.
+# ACTION: Install mixer and volume control
 # DEFAULT: y
 
 # Check root
@@ -8,33 +7,11 @@
 
 base_dir="$(dirname "$(readlink -f "$0")")"
 
-
-# INSTALL OPENBOX AND DEPENDENCES
+# Install packages
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
-apt-get install -y openbox obconf obmenu xinit lxappearance compton pnmixer pavucontrol xfce4-clipman 
+apt-get install -y pnmixer pavucontrol
 
-
-# COPY OPENBOX THEME
-tar -xzvf "$base_dir"/openbox_theme.tgz -C /usr/share/themes/
-
-
-# COPY OPENBOX CONFIG FILES
-for d in /etc/skel /home/*/; do
-    # Skip dirs in /home that not are user home
-    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
-
-	# Create config folders if no exists
-	d="$d/.config/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
-	d="$d/openbox/";  [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
-
-	# Copy openbox config file
-	cp -v "$base_dir/rc.xml" "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d/rc.xml"
-	# Copy openbox autostart file
-	cp -v "$base_dir/autostart" "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d/autostart"
-done
-
-
-# pnmixer config
+# pnmixer config icon
 for d in /etc/skel /home/*/; do
     # Skip dirs in /home that not are user home
     [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
