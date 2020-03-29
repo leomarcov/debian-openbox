1944f315e25d82b4aa1138ac661eeff8d6e61030
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 01:25:17 +0100

    Update install.sh

--- config_bash-aliases/install.sh
+++ config_bash-aliases/install.sh
@@ -6,7 +6,7 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 base_dir="$(dirname "$(readlink -f "$0")")"
-comment_mark="#DEBIAN-OPENBOX-SCRIPT"
+comment_mark="#DEBIAN-OPENBOX"
 
 for d in  /home/*/  /usr/etc/skel/  /root; do
 	# Skip dirs in /home that not are user home
