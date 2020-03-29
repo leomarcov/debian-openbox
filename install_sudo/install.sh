46b60f0dec20f9ec82a03d84160aea1aa1656293
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 11:13:10 +0200

    Update install.sh

--- install_sudo/install.sh
+++ install_sudo/install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# ACTION: Config useful aliases
+# ACTION: Install sudo and add user 1000 to sudo
 # DEFAULT: y
 
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+
+find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
+apt-get install -y sudo
+
+# Add user 1000 to sudo group
+user=$(cat /etc/passwd | cut -f 1,3 -d: | grep :1000$ | cut -f1 -d:)
+[ "$user" ] && adduser "$user" sudo
