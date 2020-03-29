bf97b4c631d23ce16988eb2bafd7550842fd6ac9
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 01:30:43 +0100

    Update install.sh

--- config_bash-prompt/install.sh
+++ config_bash-prompt/install.sh
@@ -6,9 +6,12 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 base_dir="$(dirname "$(readlink -f "$0")")"
-comment_mark="#BL-POSTINSTALL"
+comment_mark="#DEBIAN-OPENBOX"
 
-for d in /home/*  /etc/skel  /root; do
+for d in /home/*  /etc/skel/  /root; do
+	# Skip dirs in /home that not are user home
+	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+	
 	# Delete previous lines added
 	sed -i "/$comment_mark/Id" "$d/.bashrc" 2> /dev/null
 	
