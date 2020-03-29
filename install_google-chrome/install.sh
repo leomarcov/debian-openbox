89745ac70f3a751a7a4fd2ac7481e49736a19f35
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 13:56:17 +0200

    Update install.sh

--- install_google-chrome/install.sh
+++ install_google-chrome/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ACTION: Install Google Chrome, add to repositories and set has default browser
-# DEFAULT: y
+# DEFAULT: n
 
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
