d5dcb916c9d9724afd0db65ca8476ba61a371270
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 14:26:51 +0200

    Update and rename theme_arc-gtk.sh to install.sh

---
+++ theme_arc-gtk/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# ACTION: Install theme Arc GTK and set as default
+# INFO: Arc GTK theme is a clear and cool GTK theme. 
+# DEFAULT: y
+
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+gtk_default="Arc"
+
+find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
+apt-get -y install arc-theme
+
+# Change accent color blue (#5294e2) for grey:
+find /usr/share/themes/Arc -type f -exec sed -i 's/#5294e2/#b3bcc6/g' {} \;   
+	
+for f in  /etc/skel/.gtkrc-2.0  /home/*/.gtkrc-2.0 ; do
+    # Skip dirs in /home that not are user home
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+	
+	f="$d/.gtkrc-2.0"
+	[ ! -f "$f" ] && cp -v "$base_dir/gtkrc-2.0" "$d/.gtkrc-2.0" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$f"
+	sed -i 's/^gtk-theme-name *= *.*/gtk-theme-name="'"$gtk_default"'"/' "$f"		
+
+	# Create config folders if no exists
+	d="$d/.config/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
+	d="$d/gtk-3.0/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
+	
+	f="$d/settings.ini"
+	[ ! -f "$f" ] && cp -v "$base_dir/settings.ini" "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$f"	
+	sed -i 's/^gtk-theme-name *= *.*/gtk-theme-name='"$gtk_default"'/' "$f"
+done
