398d42d1ec2b9f48cdc0045b7ba85cc9203e4083
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-30 01:06:11 +0200

    Update install.sh

--- install_some-packages/install.sh
+++ install_some-packages/install.sh
@@ -7,5 +7,10 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
-apt-get install -y vim vlc ttf-mscorefonts-installer fonts-freefont-ttf fonts-droid-fallback rar zip unzip gmtp mtp-tools mailutils traceroute acl
+
+# Install free packages
+apt-get install -y vim vlc fonts-freefont-ttf fonts-droid-fallback zip unzip gmtp mtp-tools mailutils traceroute acl gnupg2
+
+# Install non-free packages
+apt-get install ttf-mscorefonts-installer rar
   
