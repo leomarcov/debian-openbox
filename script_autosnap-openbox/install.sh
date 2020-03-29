186ddceb19daae1f4f2e5918893167289c0e590e
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 14:46:25 +0200

    Update install.sh

--- script_autosnap-openbox/install.sh
+++ script_autosnap-openbox/install.sh
@@ -7,7 +7,7 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 base_dir="$(dirname "$(readlink -f "$0")")"
-comment_mark="#BL-POSTINSTALL-autosnap"
+comment_mark="#DEBIAN-OPENBOX-autosnap"
 
 # Install dependences
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update 
@@ -16,3 +16,23 @@ apt-get -y install xdotool
 cp -v "$base_dir/autosnap" /usr/bin/
 chmod +x /usr/bin/autosnap
 
+for d in /etc/skel/.config/openbox/rc.xml  /home/*/.config/openbox/rc.xml; do
+    # Skip dirs in /home that not are user home
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+	
+	f="$d/.config/openbox/rc.xml"
+	# Skip if Openbox not installed
+	[ ! -f "$f" ] && continue
+	
+	# Delete all previous lines added
+	sed -i "/${comment_mark}/Id" "$f"
+
+	# Add keybinds por each autosnap command
+	rc="$(sed '/<keyboard>/q' "$f"; cat "$base_dir/keybinds_rc.xml"; sed -n -e '/<keyboard>/,$p' "$f" | tail +2 )"
+	echo "$rc" > "$f"
+
+	# Add context titlebar
+	sed -i '/<context name="Titlebar">/,/<\/context>/{//!d}' "$f"	# Delete current context titlebar
+	rc="$(sed '/<context name="Titlebar">/q' "$f"; cat "$base_dir/titlebar_rc.xml"; sed -n -e '/<context name="Titlebar">/,$p' "$f" | tail +2)"
+	echo "$rc" > "$f"
+done
