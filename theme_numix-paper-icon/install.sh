0c6717e8965405eb20e0ecfd6488b4e111b33592
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-29 14:31:15 +0200

    Update install.sh

--- theme_numix-paper-icon/install.sh
+++ theme_numix-paper-icon/install.sh
@@ -3,6 +3,9 @@
 # INFO: Numix-Paper is a icon theme based on Numix and Paper icon themes.
 # DEFAULT: y
 
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
 base_dir="$(dirname "$(readlink -f "$0")")"
 icon_default="Numix-Paper"
 
