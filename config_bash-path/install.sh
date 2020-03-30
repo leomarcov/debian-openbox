330b3a5e73599ed21443cd338bedf3880bd4f49c
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-03-30 21:03:43 +0200

    Update and rename config_path/install.sh to config_bash-path/install.sh

---
+++ config_bash-path/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ACTION: Copy modified .profile file with new path and color definitions
+# DEFAULT: y
+
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+base_dir="$(dirname "$(readlink -f "$0")")"
+
+for d in  /home/*/  /etc/skel/  /root; do
+	# Skip dirs in /home that not are user home
+	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+    
+	cp -v "$base_dir/profile" "$d/.profile"
+done
