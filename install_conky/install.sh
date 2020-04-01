b85d1ae7b259948fdbd90f8c8fd39631ed98b1b2
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-01 13:50:34 +0200

    Update install.sh

--- install_conky/install.sh
+++ install_conky/install.sh
@@ -7,13 +7,20 @@
 
 base_dir="$(dirname "$(readlink -f "$0")")"
 
-for d in /usr/share/bunsen/skel/  /home/*/ ; do
+# Install conky
+find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
+apt-get install -y conky
+
+for d in /etc/skel/  /home/*/ ; do
+    # Skip dirs in /home that not are user home
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+	
 	cp -v "$base_dir"/conkyrc "$d/.conkyrc"
 done
 
 # Show /home in conky if /home has mounted in separated partition
 if mount | grep -q "on /home[[:blank:]]"; then
-for f in  /usr/share/bunsen/skel/.conkyrc  /home/*/.conkyrc; do
+for f in  /etc/skel/.conkyrc  /home/*/.conkyrc; do
 	sed -i '/Home usage/s/^#//g' "$f"
 done
 fi
