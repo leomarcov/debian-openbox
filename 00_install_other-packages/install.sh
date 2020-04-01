27c89a5fecb6441bd41f5a9501528d106a131178
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-01 17:42:16 +0200

    Update install.sh

--- 00_install_other-packages/install.sh
+++ 00_install_other-packages/install.sh
@@ -7,11 +7,22 @@
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
+# Add contrib and non-free sections
+deb_lines="deb http://deb.debian.org/debian/ buster main
+deb-src http://deb.debian.org/debian/ buster main
+deb http://security.debian.org/debian-security buster/updates main
+deb-src http://security.debian.org/debian-security buster/updates main
+deb http://deb.debian.org/debian/ buster-updates main
+deb-src http://deb.debian.org/debian/ buster-updates main"
 
+(
+IFS=$'\n'
+for l in $deb_lines; do
+	sed -i "s\\^$l$\\$l contrib non-free\\" /etc/apt/sources.list
+done
+)
+
+
+# Update and install packages
 apt-get update  
-
-# Install free packages
-apt-get install -y vim vlc fonts-freefont-ttf fonts-droid-fallback zip unzip gmtp mtp-tools mailutils traceroute acl gnupg2 synaptic galternatives yad
-
-# Install non-free packages
 apt-get install ttf-mscorefonts-installer rar
