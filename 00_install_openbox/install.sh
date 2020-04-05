3a2ac2399d29940db6aa55506bd68ba48b79549d
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-05 18:20:21 +0200

    Update install.sh

--- 00_install_openbox/install.sh
+++ 00_install_openbox/install.sh
@@ -18,7 +18,7 @@ systemctl disable NetworkManager-wait-online.service
 
 # COPY OPENBOX THEME
 tar -xzvf "$base_dir"/openbox_theme.tgz -C /usr/share/themes/
-
+cp -r "$base_dir/openbox_menu" /usr/share/icons/
 
 # COPY OPENBOX CONFIG FILES
 for d in /etc/skel /home/*/; do
