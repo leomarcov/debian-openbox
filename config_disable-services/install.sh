d4d86919c4b6c5e90322d600a5a2f09667d6b41c
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-07 11:35:36 +0200

    Create install.sh

---
+++ config_disable-services/install.sh
@@ -0,0 +1,14 @@
+ 
+#!/bin/bash
+# ACTION: Disable some unnecessary services for not start during boot time
+# INFO: Services are: NetworkManager-wait-online.service, ModemManager.service, pppd-dns.service
+# DEFAULT: y
+
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+systemctl mask NetworkManager-wait-online.service
+systemctl status NetworkManager.service &>/dev/null && systemctl networking disable
+systemctl disable wpa_supplicant
+systemctl disable ModemManager.service
+systemctl disable pppd-dns.service
