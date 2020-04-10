c38a8f1126996db8c3f15fb136563b9c2bdb660f
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-10 14:06:13 +0200

    Update install.sh

--- theme_fonts/install.sh
+++ theme_fonts/install.sh
@@ -10,6 +10,7 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update 
 apt-get install -y fonts-cantarell fonts-liberation fonts-opensymbol fonts-noto-core fonts-noto-mono fonts-inconsolata
+apt-get remove -y fonts-droid-fallback
 
 [ ! -d /usr/share/fonts/extra ] && mkdir /usr/share/fonts/extra/
 tar -xzvf "$base_dir"/fonts.tgz -C /usr/share/fonts/extra/
