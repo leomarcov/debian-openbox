db6469813637cc9d721960972b98cf8a693357b7
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 01:57:02 +0200

    Update install.sh

--- config_boot-messages/install.sh
+++ config_boot-messages/install.sh
@@ -3,10 +3,11 @@
 # INFO: In boot process the system can show a stupid logo or messages about the booting process.
 # DEFAULT: y
 
+# Config variables
+base_dir="$(dirname "$(readlink -f "$0")")"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
-
-base_dir="$(dirname "$(readlink -f "$0")")"
 
 # Delete existing lines
 for i in $(cat "$base_dir/grub.conf"  | cut -f1 -d=); do
