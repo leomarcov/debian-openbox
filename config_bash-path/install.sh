54af6e1434f74f001eb9a68db39bd2abf46145bb
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 01:52:12 +0200

    Update install.sh

--- config_bash-path/install.sh
+++ config_bash-path/install.sh
@@ -2,14 +2,14 @@
 # ACTION: Config modified .profile file with new path and color definitions
 # DEFAULT: y
 
+# Config variables
+base_dir="$(dirname "$(readlink -f "$0")")"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
-base_dir="$(dirname "$(readlink -f "$0")")"
-
 for d in  /home/*/  /etc/skel/  /root; do
-	# Skip dirs in /home that not are user home
-	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes 
     
 	cp -v "$base_dir/profile" "$d/.profile"
 done
