743dbfa20791f33c04cae4507d687f3491e4fe27
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 01:53:48 +0200

    Update install.sh

--- config_bash-prompt/install.sh
+++ config_bash-prompt/install.sh
@@ -2,15 +2,15 @@
 # ACTION: Config new bash prompt
 # DEFAULT: y
 
-# Check root
-[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
-
+# Config variables
 base_dir="$(dirname "$(readlink -f "$0")")"
 comment_mark="#DEBIAN-OPENBOX"
 
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
 for d in /home/*  /etc/skel/  /root; do
-	# Skip dirs in /home that not are user home
-	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes 
 	
 	# Delete previous lines added
 	sed -i "/$comment_mark/Id" "$d/.bashrc" 2> /dev/null
