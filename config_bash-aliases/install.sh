d985cdc61b21c919e5359262395cc81b81fd39ca
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 01:50:31 +0200

    Update install.sh

--- config_bash-aliases/install.sh
+++ config_bash-aliases/install.sh
@@ -2,20 +2,21 @@
 # ACTION: Config useful aliases
 # DEFAULT: y
 
-# Check root
-[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
-
+# Config variables
 base_dir="$(dirname "$(readlink -f "$0")")"
 comment_mark="#DEBIAN-OPENBOX"
 
-for d in  /home/*/  /etc/skel/  /root; do
-	# Skip dirs in /home that not are user home
-	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+for d in  /etc/skel/  /root  /home/*/; do
+	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes 
     
-	sed -i "/$comment_mark/Id" "$d/.bash_aliases" 2> /dev/null
-	cp -v "$base_dir/bash_aliases" "$d/.bash_aliases"
+	sed -i "/$comment_mark/Id" "$d/.bash_aliases" 2>/dev/null
+	cat "$base_dir/bash_aliases" >> "$d/.bash_aliases"
 done
 
+# Load .bash_aliases from root .bashrc
 grep -q ".bash_aliases" /root/.bashrc || echo '
 if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
