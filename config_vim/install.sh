698a75a2a491b92ad389afb3bb5005239bcd122d
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 01:59:51 +0200

    Update install.sh

--- config_vim/install.sh
+++ config_vim/install.sh
@@ -2,19 +2,18 @@
 # ACTION: Config vim with custom configs
 # DEFAULT: y
 
+# Config variables
+base_dir="$(dirname "$(readlink -f "$0")")"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
-
-base_dir="$(dirname "$(readlink -f "$0")")"
 
 # Install vim
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
 apt-get install -y vim
 
-
 for d in  /etc/skel/  /home/*/ /root/; do
-    # Skip dirs in /home that not are user home
-    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes 
 	
 	cp -v "$base_dir/vimrc" "$d/.vimrc" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d/.vimrc"
 done
