901f546d5720c089321275b212ec7f00195fc129
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:02:03 +0200

    Update install.sh

--- config_home-perms/install.sh
+++ config_home-perms/install.sh
@@ -4,17 +4,17 @@
 # INFO: By default permissions are 755 and has access permissions to all users
 # DEFAULT: y
 
+# Config variables
+base_dir="$(dirname "$(readlink -f "$0")")"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
-
-base_dir="$(dirname "$(readlink -f "$0")")"
 
 # Config adduser for create users with $HOME permisions 0750
 [ -f /etc/adduser.conf ] && sed -i 's/DIR_MODE=[0-9]*/DIR_MODE=0750/g' /etc/adduser.conf
 
 for d in  /home/*/; do
-    # Skip dirs in /home that not are user home
-    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes 
 	
 	# Set current home permissions
 	chmod -v 0750 "$d"
