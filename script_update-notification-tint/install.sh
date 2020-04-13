91a36b4d04609d09ec60dd43f63d91c6d7b548a4
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 11:56:31 +0200

    Update install.sh

--- script_update-notification-tint/install.sh
+++ script_update-notification-tint/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# ACTION: Install script update-notification
-# INFO: Script update-notification.sh checks periodically APT updates and show in tint2 bar
+# ACTION: Install script update-notification for check periodically APT updates and show in tint2 bar executor
 # DEFAULT: y
 
 base_dir="$(dirname "$(readlink -f "$0")")"
