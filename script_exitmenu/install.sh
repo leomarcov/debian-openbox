c1b691a6edcd0a071751271d7706074d9905d6e2
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:33:16 +0200

    Update install.sh

--- script_exitmenu/install.sh
+++ script_exitmenu/install.sh
@@ -1,23 +1,24 @@
 # ACTION: Install script obexit with exit-power menu based on rofi
 # DEFAULT: y
 
+# Config variables
+base_dir="$(dirname "$(readlink -f "$0")")"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
-
-base_dir="$(dirname "$(readlink -f "$0")")"
 
 # Install fonts-noto for utf-8 symbols
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update 
 apt-get install -y fonts-noto-core
 
-
+# Copy obexit script
 f="obexit"
 cp -v "$base_dir/$f" /usr/bin
 chmod +x "/usr/bin/$f"
 
+# Copy users config
 for d in /etc/skel/  /home/*/; do
-    # Skip dirs in /home that not are user home
-    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes
 
 	# Create config folders if no exists
 	d2="$d"
