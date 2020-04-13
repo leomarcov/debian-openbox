40648b9290c6322e55451cf3ee13b6e2471ad1da
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:13:38 +0200

    Update install.sh

--- install_terminator/install.sh
+++ install_terminator/install.sh
@@ -2,18 +2,20 @@
 # ACTION: Install Termiantor terminal and config theme
 # DEFAULT: y
 
+# Config variables
+base_dir="$(dirname "$(readlink -f "$0")")"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
-base_dir="$(dirname "$(readlink -f "$0")")"
-
-
-# INSTALL OPENOX AND DEPENDENCES
+# Install packages
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
 apt-get install -y terminator
 
-# INSTALL CONFIG
+# Copy users config
 for d in /etc/skel/  /home/*/; do
+	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes 
+
 	# Create config folders if no exists
 	d="$d/.config/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
 	d="$d/terminator/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
