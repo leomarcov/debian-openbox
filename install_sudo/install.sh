be5840dce56f1bf4d6e9379cda0019acb1adad09
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:12:24 +0200

    Update install.sh

--- install_sudo/install.sh
+++ install_sudo/install.sh
@@ -5,7 +5,7 @@
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
-
+# Install packages
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
 apt-get install -y sudo
 
