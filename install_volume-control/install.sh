e314c16b306ddf4aa15392576f9de483fe77cbaa
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:24:00 +0200

    Update install.sh

--- install_volume-control/install.sh
+++ install_volume-control/install.sh
@@ -2,19 +2,19 @@
 # ACTION: Install mixer and volume control
 # DEFAULT: y
 
+# Install packages
+base_dir="$(dirname "$(readlink -f "$0")")"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
-
-base_dir="$(dirname "$(readlink -f "$0")")"
 
 # Install packages
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
 apt-get install -y pnmixer pavucontrol
 
-# pnmixer config icon
+# Copy users config
 for d in /etc/skel /home/*/; do
-    # Skip dirs in /home that not are user home
-    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes
 
 	# Create config folders if no exists
 	d="$d/.config/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
