59fe620c626411d346df25bee72f7fb48587b2a5
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 14:10:05 +0200

    Update install.sh

--- install_cups/install.sh
+++ install_cups/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ACTION: Install CUPS printer system and add user 1000 to lpadmin group
 # INFO: CUPS can be managed in http://localhost:631 and admin users must be in lpadmin group
-# DEFAULT: y
+# DEFAULT: n
 
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
