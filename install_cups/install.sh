55778c911c7dab8c6cba5b06d9eef8cd20191605
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 13:23:40 +0200

    Create install.sh

---
+++ install_cups/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ACTION: Install CUPS printer system and add user 1000 to lpadmin group
+# INFO: CUPS can be managed in http://localhost:631 and users
+# DEFAULT: y
+
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
+# Install packages
+find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
+apt-get install -y cups
+
+# Add user 1000 to sudo group
+user=$(cat /etc/passwd | cut -f 1,3 -d: | grep :1000$ | cut -f1 -d:)
+[ "$user" ] && adduser "$user" lpqadmin
