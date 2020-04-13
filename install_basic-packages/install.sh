93ce24a533786d2556370226ff363f2b9ffd5876
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:03:56 +0200

    Update install.sh

--- install_basic-packages/install.sh
+++ install_basic-packages/install.sh
@@ -6,9 +6,8 @@
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
-find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
-
 # Install free packages
+find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
 apt-get install -y vim vlc zip unzip gmtp mtp-tools mailutils traceroute acl gnupg2 synaptic galternatives yad mlocate evince
 
   
