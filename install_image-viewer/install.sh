01ac370e638030e92df4a1c3bd082fd4881a6f97
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:07:24 +0200

    Update install.sh

--- install_image-viewer/install.sh
+++ install_image-viewer/install.sh
@@ -2,16 +2,16 @@
 # ACTION: Install nomacs image viewer and configs
 # DEFAULT: y
 
+# Config variables
+base_dir="$(dirname "$(readlink -f "$0")")"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
-base_dir="$(dirname "$(readlink -f "$0")")"
-
-# INSTALL OPENOX AND DEPENDENCES
+# Install packages
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
 apt-get install -y nomacs
 
-# INSTALL CONFIG
 for d in /etc/skel/  /home/*/; do
 	# Create config folders if no exists
 	d="$d/.config/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
