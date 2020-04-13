0b16b5c48523af75d9284daebbf753552751c008
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:34:10 +0200

    Update install.sh

--- script_tty-login-neofetch/install.sh
+++ script_tty-login-neofetch/install.sh
@@ -3,11 +3,12 @@
 # INFO: Login in text mode is cool and nerd. You can use tt1 login and physlock for enter your password when go back from suspend and lock screen. 
 # DEFAULT: y
 
+# Config variables
+base_dir="$(dirname "$(readlink -f "$0")")"
+comment_mark="#DEBIAN-OPENBOX-ttyloginneofetch"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
-
-comment_mark="#DEBIAN-OPENBOX-ttyloginneofetch"
-base_dir="$(dirname "$(readlink -f "$0")")"
 
 # Config runlevel 3
 systemctl set-default multi-user.target
@@ -35,7 +36,6 @@ chmod -v a+x /usr/bin/neofetch_issue.sh
 # Config getty to run neofetch_issue.sh every time tty start:
 echo '[Service]
 ExecStartPre=-/bin/bash -c "/usr/bin/neofetch_issue.sh"' | tee "/etc/systemd/system/getty@.service.d/override.conf"
-
 
 # Add physlock as x-locker alternative
 update-alternatives --install /usr/bin/x-locker x-locker $(which physlock) 90
