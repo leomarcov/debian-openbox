247d28f64c90dfb59a263a1c39dc9002cd1b56d3
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:25:38 +0200

    Update install.sh

--- install_conky/install.sh
+++ install_conky/install.sh
@@ -12,10 +12,9 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
 apt-get install -y conky
 
-
+# Copy users config
 for d in /etc/skel/  /home/*/ ; do
-    # Skip dirs in /home that not are user home
-    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes
 
 	# Create config folders if no exists
 	d="$d/.config/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
@@ -32,7 +31,6 @@ for f in  /etc/skel/.config/conky/basic.conkyrc  /home/*/.config/conky/basic.con
 	sed -i '/Home usage/s/^#//g' "$f"
 done
 fi
-
 
 # Copy conky-session
 f="conky-session"
