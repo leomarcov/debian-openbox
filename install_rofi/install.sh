1aedb03bc6e5ca1d41dcb03c2dfd4c3a28be9957
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 02:11:46 +0200

    Update install.sh

--- install_rofi/install.sh
+++ install_rofi/install.sh
@@ -3,21 +3,21 @@
 # INFO: Rofi is a simple text switcher and launcher
 # DEFAULT: y
 
+# Config variables
+base_dir="$(dirname "$(readlink -f "$0")")"
+comment_mark="#DEBIAN-OPENBOX-rofi"
+rofi_command="rofi -show drun"
+
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
-comment_mark="#DEBIAN-OPENBOX-rofi"
-rofi_command="rofi -show drun"
-base_dir="$(dirname "$(readlink -f "$0")")"
-
-# Install rofi package
+# Install packages
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
 apt-get install -y rofi
 
 # Config rofi theme and run mode for all users
 for d in /etc/skel/  /home/*/; do
-    # Skip dirs in /home that not are user home
-    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue
+    [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes 
 
 	# Create config folders if no exists
 	d2="$d"
