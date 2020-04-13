383d387ddb8583ae37c4b54dfbdc229cb5f38cbd
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-13 11:52:40 +0200

    Update install.sh

--- script_autosnap/install.sh
+++ script_autosnap/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ACTION: Install script autosnap for autosnap windows with double click in titlebar
+# ACTION: Install script autosnap for half-maximize windows with mouse middle click in titlebar
 # INFO: Script autosnap half-maximize windows in Openbox WM.
 # DEFAULT: y
 
