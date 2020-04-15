b6b0d2b6850329d5315815f487c7946d39fff453
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-15 21:42:21 +0200

    Update install.sh

--- 10_openbox_terminator/install.sh
+++ 10_openbox_terminator/install.sh
@@ -10,10 +10,12 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 # Install packages
+echo -e "\e[1mInstalling packages...\e[0m"
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
 apt-get install -y terminator
 
 # Copy users config
+echo -e "\e[1mCopying configs to all users...\e[0m"
 for d in /etc/skel/  /home/*/; do
 	[ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes 
 
