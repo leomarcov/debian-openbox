848de815ad554fd5d1e20bc49f1fbabc6fdf5e64
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-16 01:12:09 +0200

    Update install.sh

--- 10_openbox_nomacs-viewer/install.sh
+++ 10_openbox_nomacs-viewer/install.sh
@@ -10,9 +10,12 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 # Install packages
+echo -e "\e[1mInstalling packages...\e[0m"
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
 apt-get install -y nomacs
 
+# Copy users config
+echo -e "\e[1mCopying configs to all users...\e[0m"
 for d in /etc/skel/  /home/*/; do
 	# Create config folders if no exists
 	d="$d/.config/"; [ ! -d "$d" ] && mkdir -v "$d" && chown -R $(stat "$(dirname "$d")" -c %u:%g) "$d"
@@ -23,4 +26,5 @@ for d in /etc/skel/  /home/*/; do
 done
 
 # Add as x-image-viewer alternative
+echo -e "\e[1mSetting as default alternative...\e[0m"
 update-alternatives --install /usr/bin/x-image-viewer x-image-viewer $(which nomacs) 90
