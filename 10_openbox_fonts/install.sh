1449c247098e4b31b6d9ec857d8faaffcd1f982b
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-04-16 01:11:22 +0200

    Update install.sh

--- 10_openbox_fonts/install.sh
+++ 10_openbox_fonts/install.sh
@@ -10,10 +10,12 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 # Install packages
+echo -e "\e[1mInstalling packages...\e[0m"
 find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update 
 apt-get install -y fonts-droid-fallback fonts-cantarell fonts-liberation fonts-opensymbol fonts-noto-core fonts-noto-mono fonts-inconsolata
 
 # Copy fonts
+echo -e "\e[1mCopying fonts...\e[0m"
 [ ! -d /usr/share/fonts/extra ] && mkdir /usr/share/fonts/extra/
 tar -xzvf "$base_dir"/fonts.tgz -C /usr/share/fonts/extra/
 
