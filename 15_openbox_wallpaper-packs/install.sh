2ce7ba31df47a6efcfd4446bc7db629ac5f06c54
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-05-04 09:46:52 +0200

    Update install.sh

--- 15_openbox_wallpaper-packs/install.sh
+++ 15_openbox_wallpaper-packs/install.sh
@@ -28,7 +28,7 @@ fi
 # Install nitrogen
 if ! which nitrogen &>/dev/null; then
 	echo -e "\e[1mInstalling packages...\e[0m"
-	find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update
+	[ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
 	apt-get -y install nitrogen
 fi
 
