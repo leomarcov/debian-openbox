bdf82b8d230fe43d8ab7fb85b38a5956a385e2b9
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-05-04 09:43:12 +0200

    Update install.sh

--- 10_openbox_rofi/install.sh
+++ 10_openbox_rofi/install.sh
@@ -13,7 +13,7 @@ rofi_command="rofi -show drun"
 
 # Install packages
 echo -e "\e[1mInstalling packages...\e[0m"
-find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
+[ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
 apt-get install -y rofi
 
 
