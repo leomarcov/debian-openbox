af3d957a95319c10cff8b8ddb7a6420577186448
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-05-04 09:46:03 +0200

    Update install.sh

--- config_sudo/install.sh
+++ config_sudo/install.sh
@@ -8,7 +8,7 @@
 
 # Install packages
 echo -e "\e[1mInstalling packages...\e[0m"
-find /var/cache/apt/pkgcache.bin -mtime 0 &>/dev/null ||  apt-get update  
+[ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
 apt-get install -y sudo
 
 # Add user 1000 to sudo group
