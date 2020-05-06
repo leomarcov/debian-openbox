d5f709c55646f3c38e3309888761d064e82cbb0f
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-05-06 15:03:34 +0200

    Update install.sh

--- config_vim/install.sh
+++ config_vim/install.sh
@@ -5,6 +5,7 @@
 
 # Config variables
 base_dir="$(dirname "$(readlink -f "$0")")"
+comment_mark='"DEBIAN-OPENBOX-vim'
 
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
@@ -12,7 +13,22 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 # Install vim
 echo -e "\e[1mInstalling packages...\e[0m"
 [ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
-apt-get install -y vim
+apt-get install -y vim vim-nox
+
+# Config vim plug for global (all users)
+echo -e "\e[1mInstalling vim plugins for all users in /etc/vim/ ...\e[0m"
+mkdir -vp "/etc/vim/autoload"
+curl -fLo /etc/vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p "/etc/vim/plugged/"
+if [ -s "/etc/vim/vimrc.local" ]; then
+	f=/etc/vim/vimrc.local
+	sed -i "/${comment_mark}/Id" "$f" 	# Delete all previous lines added
+	cat "$base_dir/vimrc.local" /etc/vim/vimrc.local > /etc/vim/vimrc.local
+else
+	cp -v "$base_dir/vimrc.local" /etc/vim/
+fi
+vim +'PlugInstall --sync' +qa
+
 
 # Copy users config
 echo -e "\e[1mSetting configs to all users...\e[0m"
