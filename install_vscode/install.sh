f3b26021340f169ee5fd6f3291a5c19a97eb076c
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-05-10 02:23:27 +0200

    Update install.sh

--- install_vscode/install.sh
+++ install_vscode/install.sh
@@ -7,20 +7,15 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 # Install repositories and update
-if ! grep -R "download.sublimetext.com" /etc/apt/ &> /dev/null; then
+if ! grep -R "vscode" /etc/apt/ &> /dev/null; then
 	echo -e "\e[1mConfiguring repositories...\e[0m"
-	echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+	echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" | tee /etc/apt/sources.list.d/vscode.list
+	wget -qO - https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 	apt-get update
 fi
 
 # Install package
 echo -e "\e[1mInstalling packages...\e[0m"
-apt-get install sublime-text || exit 1
+apt-get install code || exit 1
 
-# Set as default
-echo -e "\e[1mSetting as default alternative...\e[0m"
-update-alternatives --install /usr/bin/x-text-editor x-text-editor /usr/bin/subl 90 && \
-update-alternatives --set x-text-editor /usr/bin/subl
 
-wget -qO - https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
