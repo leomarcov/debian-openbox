c34b3c6c4c6ff6ee300ed2548eeda25194bb2468
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-11-28 11:57:27 +0100

    Update install.sh

--- 00_openbox_install-core/install.sh
+++ 00_openbox_install-core/install.sh
@@ -12,7 +12,7 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 # Install packages
 echo -e "\e[1mInstalling packages...\e[0m"
 [ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
-apt-get install -y openbox obconf obmenu xinit lxappearance compton xfce4-screenshooter xfce4-clipman xfce4-power-manager arandr libexo-1-0 gsimplecal xcape gparted file-roller xautomation python-xdg yad
+apt-get install -y openbox obconf obmenu xinit lxappearance compton xfce4-screenshooter xfce4-clipman xfce4-power-manager arandr libexo-1-0 gsimplecal xcape gparted file-roller xautomation python-xdg yad inxi
 apt-get install -y network-manager network-manager-gnome
 
 echo -e "\e[1mCopying themes and tools...\e[0m"
