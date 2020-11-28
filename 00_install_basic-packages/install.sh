ed0733412a03adaf7cd505c584737a67e8cbaef1
leomarcov <32820131+leomarcov@users.noreply.github.com>
2020-11-28 11:58:09 +0100

    Update install.sh

--- 00_install_basic-packages/install.sh
+++ 00_install_basic-packages/install.sh
@@ -10,6 +10,6 @@
 # Install free packages
 echo -e "\e[1mInstalling packages...\e[0m"
 [ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
-apt-get install -y vim vlc zip unzip gmtp mtp-tools mailutils traceroute acl gnupg2 synaptic galternatives mlocate evince apt-transport-https curl
+apt-get install -y vim vlc zip unzip gmtp mtp-tools mailutils traceroute acl gnupg2 synaptic galternatives mlocate evince apt-transport-https curl rar unrar
 
   
