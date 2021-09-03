a2fedf1141be7433c43d4564fc290d7eda11d546
leomarcov <32820131+leomarcov@users.noreply.github.com>
2021-09-03 11:31:20 +0200

    Update install.sh

--- 00_config_add-contrib-nonfree/install.sh
+++ 00_config_add-contrib-nonfree/install.sh
@@ -7,19 +7,23 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 # Add contrib and non-free sections
-deb_lines="deb http://deb.debian.org/debian/ buster main
-deb-src http://deb.debian.org/debian/ buster main
-deb http://security.debian.org/debian-security buster/updates main
-deb-src http://security.debian.org/debian-security buster/updates main
-deb http://deb.debian.org/debian/ buster-updates main
-deb-src http://deb.debian.org/debian/ buster-updates main"
-
+deb_lines_contrib="$(egrep '^(deb|deb-src) (http://deb.debian.org/debian/|http://security.debian.org/debian-security)' | grep -v contrib)"
+deb_lines_nonfree="$(egrep '^(deb|deb-src) (http://deb.debian.org/debian/|http://security.debian.org/debian-security)' | grep -v non-free)"
 (
+
 IFS=$'\n'
-for l in $deb_lines; do
-	sed -i "s\\^$l$\\$l contrib non-free\\" /etc/apt/sources.list
+# Add contrib 
+for l in $deb_lines_contrib; do
+	sed -i "s\\^$l$\\$l contrib\\" /etc/apt/sources.list
 done
 )
+
+# Add non-free
+for l in $deb_lines_nonfree; do
+	sed -i "s\\^$l$\\$l non-free\\" /etc/apt/sources.list
+done
+)
+
 
 # Update and install packages
 apt-get update  
