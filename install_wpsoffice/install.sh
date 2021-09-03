127fcc4f603a691525a59c85817e296ea0ac5508
leomarcov <32820131+leomarcov@users.noreply.github.com>
2021-09-03 10:46:29 +0200

    Update install.sh

--- install_wpsoffice/install.sh
+++ install_wpsoffice/install.sh
@@ -10,7 +10,7 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 echo -e "\e[1mDownloading packages...\e[0m"
-url="http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9126/wps-office_11.1.0.9126.XA_amd64.deb"
+url="https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10702/wps-office_11.1.0.10702.XA_amd64.deb"
 t=$(mktemp -d)
 wget -P "$t" "$url"  
 if [ $? -eq 0 ]; then
