8fa8c0a9c5084d0da710319e3967e2ded2fe5b5a
leomarcov <32820131+leomarcov@users.noreply.github.com>
2021-09-03 11:33:46 +0200

    Update install.sh

--- 00_config_add-contrib-nonfree/install.sh
+++ 00_config_add-contrib-nonfree/install.sh
@@ -7,8 +7,8 @@
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 # Add contrib and non-free sections
-deb_lines_contrib="$(egrep '^(deb|deb-src) (http://deb.debian.org/debian/|http://security.debian.org/debian-security)' | grep -v contrib)"
-deb_lines_nonfree="$(egrep '^(deb|deb-src) (http://deb.debian.org/debian/|http://security.debian.org/debian-security)' | grep -v non-free)"
+deb_lines_contrib="$(egrep '^(deb|deb-src) (http://deb.debian.org/debian/|http://security.debian.org/debian-security)' /etc/apt/sources.list | grep -v contrib /etc/apt/sources.list)"
+deb_lines_nonfree="$(egrep '^(deb|deb-src) (http://deb.debian.org/debian/|http://security.debian.org/debian-security)' /etc/apt/sources.list | grep -v non-free /etc/apt/sources.list)"
 (
 
 IFS=$'\n'
