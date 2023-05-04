88cc478529749a8289fb12725e7d085f197ecfb8
leomarcov <32820131+leomarcov@users.noreply.github.com>
2023-05-04 13:54:49 +0200

    Update install.sh

--- config_disable-services/install.sh
+++ config_disable-services/install.sh
@@ -6,23 +6,23 @@
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
-systemctl stop NetworkManager-wait-online.service
-systemctl mask NetworkManager-wait-online.service
+#systemctl stop NetworkManager-wait-online.service
+#systemctl mask NetworkManager-wait-online.service
 
-systemctl stop wpa_supplicant
-systemctl disable wpa_supplicant	# No mask, may be needed by network manager
+#systemctl stop wpa_supplicant
+#systemctl disable wpa_supplicant	# No mask, may be needed by network manager
 
 systemctl stop ModemManager.service
 systemctl disable ModemManager.service
 
-systemctl stop pppd-dns.service
-systemctl disable pppd-dns.service
+#systemctl stop pppd-dns.service
+#systemctl disable pppd-dns.service
 
-if systemctl status NetworkManager.service &>/dev/null; then
-	#apt-get purge ifupdown; rm -rf /etc/network/*
-	systemctl networking disable
+#if systemctl status NetworkManager.service &>/dev/null; then
+#	#apt-get purge ifupdown; rm -rf /etc/network/*
+#	systemctl networking disable
 
 	#apt-get purge network-dispacher
-	systemctl stop systemd-networkd.service
-	systemctl disable systemd-networkd.service
-fi
+#	systemctl stop systemd-networkd.service
+#	systemctl disable systemd-networkd.service
+#fi
