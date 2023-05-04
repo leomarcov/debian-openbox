ab144f9a676c72e06e52d81e1563655a535fe4c7
leomarcov <32820131+leomarcov@users.noreply.github.com>
2023-05-04 12:15:54 +0200

    Update install.sh

--- install_virtualbox/install.sh
+++ install_virtualbox/install.sh
@@ -5,7 +5,7 @@
 
 # Config variables
 vb_package="virtualbox-6.1"
-ep_url="https://download.virtualbox.org/virtualbox/6.1.34/Oracle_VM_VirtualBox_Extension_Pack-6.1.34.vbox-extpack"
+ep_url="https://download.virtualbox.org/virtualbox/7.0.8/Oracle_VM_VirtualBox_Extension_Pack-7.0.8.vbox-extpack"
 main_distro="$(cat /etc/apt/sources.list | grep ^deb | awk '{print $3}' | head -1)"
 
 # Check root
