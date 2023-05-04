827e397f4c7233bee52418f59ad25cfda8b0091c
leomarcov <32820131+leomarcov@users.noreply.github.com>
2023-05-04 13:09:55 +0200

    Update install.sh

--- 00_install_basic-cli-packages/install.sh
+++ 00_install_basic-cli-packages/install.sh
@@ -10,6 +10,6 @@
 # Install free packages
 echo -e "\e[1mInstalling packages...\e[0m"
 [ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
-apt-get install -y vim zip unzip gmtp mtp-tools mailutils traceroute acl gnupg2 mlocate apt-transport-https curl rar unrar ntfs-3g
+apt-get install -y vim zip unzip mailutils traceroute acl gnupg2 mlocate apt-transport-https curl rar unrar ntfs-3g
 apt-get install -y firmware-linux-nonfree
   
