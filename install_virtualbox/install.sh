9a9e12d9f1065db377522eeee754a09201aa28ac
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-20 17:54:25 +0100

    Update install.sh

--- install_virtualbox/install.sh
+++ install_virtualbox/install.sh
@@ -3,16 +3,12 @@
 # INFO: VirtualBox is a free opensource hosted hypervisor
 # DEFAULT: y
 
-# Config variables
-vb_package="virtualbox-7.2"
-ep_url="https://download.virtualbox.org/virtualbox/7.2.6/Oracle_VM_VirtualBox_Extension_Pack-7.2.6.vbox-extpack"
-main_distro="$(cat /etc/apt/sources.list | grep ^deb | awk '{print $3}' | head -1)"
-
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 # Install repositories and update
 if ! grep -R "download.virtualbox.org" /etc/apt/ &> /dev/null; then
+	main_distro="$(cat /etc/apt/sources.list | grep ^deb | awk '{print $3}' | head -1)"
 	echo -e "\e[1mConfiguring repositories...\e[0m"	
 	wget -qO - "https://www.virtualbox.org/download/oracle_vbox_2016.asc" | gpg --dearmor --yes -o /usr/share/keyrings/virtualbox-keyring.gpg
 	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/virtualbox-keyring.gpg] http://download.virtualbox.org/virtualbox/debian $main_distro contrib" | tee /etc/apt/sources.list.d/virtualbox.list
@@ -21,6 +17,8 @@ fi
 
 # Install packages
 echo -e "\e[1mInstalling packages...\e[0m"
+vb_package=$(apt-cache search virtualbox | grep "^virtualbox-[0-9]" | cut -f1 -d" " | sort -n | tail -1)
+[ ! "$vb_package ] || { echo "VirtualBox package not found"; exit 1; }
 [ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
 apt-get install -y linux-headers-$(uname -r) "$vb_package" || exit 1
 
@@ -40,6 +38,9 @@ if ! which vboxmanage &> /dev/null; then
 fi
 
 # Install extension pack
+vb_version=$(vboxmanage --version | grep -Eo "^[0-9]\.[0-9]+\.[0-9]+")
+ep_url="https://download.virtualbox.org/virtualbox/${vb_version}/Oracle_VM_VirtualBox_Extension_Pack-${vb_version}.vbox-extpack"
+
 echo -e "\e[1mDownloading and installing Extension pack..\e[0m"
 t=$(mktemp -d)
 wget -P "$t" "$ep_url"  
