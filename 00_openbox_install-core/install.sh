2aedb31d187fda860d0d731165e7f76cf24fc507
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-20 22:45:54 +0100

    Update install.sh

--- 00_openbox_install-core/install.sh
+++ 00_openbox_install-core/install.sh
@@ -19,7 +19,7 @@ apt-get install -y network-manager network-manager-gnome
 if [ "$(systemd-detect-virt)" = "oracle" ]; then
 	vb_version=$(wget -qO- https://download.virtualbox.org/virtualbox/LATEST.TXT)
 	echo -e "\e[1mVirtualBox guest detected. Installing Guest Additions ${vb_version} ...\e[0m"
-	apt install -y build-essential dkms linux-headers-$(uname -r) wget p7zip-full
+	apt-get install -y build-essential dkms linux-headers-$(uname -r) wget p7zip-full
 	t=$(mktemp -d)
 	ga_url="https://download.virtualbox.org/virtualbox/${vb_version}/VBoxGuestAdditions_${vb_version}.iso"
 	wget -P "$t" "$ga_url"
