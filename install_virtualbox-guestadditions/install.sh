d26fd99f5fdab3b8ba81e979573fc318ad270e18
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-22 17:58:36 +0100

    Create install.sh

---
+++ install_virtualbox-guestadditions/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# ACTION: Install VirtualBox Gust Additions
+# INFO: VirtualBox Gust Additions is a bundle of device drivers and system applications installed inside a virtual machine to improve performance, graphics, and usability.
+# DEFAULT: n
+
+# Check system run on VirtualBox VM
+if [ "$(systemd-detect-virt)" = "oracle" ]; then
+  echo "Current system not running on VirtualBox guest VM" 1>&2
+  exit 1
+fi
+
+vb_version=$(wget -qO- https://download.virtualbox.org/virtualbox/LATEST.TXT)
+echo -e "\e[1mVirtualBox guest detected. Installing Guest Additions ${vb_version} ...\e[0m"
+apt-get install -y build-essential dkms linux-headers-$(uname -r) wget p7zip-full
+t=$(mktemp -d)
+ga_url="https://download.virtualbox.org/virtualbox/${vb_version}/VBoxGuestAdditions_${vb_version}.iso"
+wget -P "$t" "$ga_url"
+7z x "$t"/* -o"$t" &>/dev/null
+bash "$t/VBoxLinuxAdditions.run"
+rm -rf "$t"
+
