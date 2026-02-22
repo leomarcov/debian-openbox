7a0a3c8fad769d5f5b03b6941c60d8fc560a1ae5
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-23 00:10:23 +0100

    Update install.sh

--- 99_clean_apt-clean/install.sh
+++ 99_clean_apt-clean/install.sh
@@ -5,6 +5,7 @@
 
 
 # Uninstall unnecesary packages
+echo -e "\e[1mUninstalling unnecesary packages...\e[0m"
 apt-get purge -y gnome-keyring modemmanager yelp xdg-desktop-portal-gnome  geoclue-2.0 popularity-contest tasksel installation-report
 apt-get purge -y fonts-noto-cjk fonts-noto-extra
 
@@ -18,9 +19,11 @@ dpkg -l | awk '/^ii/{print $2}' | xargs apt-get install --reinstall -y
  
 # Uninstall unnecesary VirtualBox guest packages
 if [ "$(systemd-detect-virt)" = "oracle" ]; then
+	echo -e "\e[1mUninstalling unnecesary firmware packages for VirtualBox guest...\e[0m"
     apt purge -y firmware-realtek firmware-atheros firmware-mediatek firmware-misc-nonfree firmware-amd-graphics
 fi
 
 # APT autoremove and clean
+echo -e "\e[1mCleaning packages and .deb files...\e[0m"
 apt-get -y autoremove --purge
 apt-get clean
