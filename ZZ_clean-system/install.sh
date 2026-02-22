3c7a7e57629bff94bc46e008d1be4a6d652ce1f2
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-22 19:51:33 +0100

    Update install.sh

--- ZZ_clean-system/install.sh
+++ ZZ_clean-system/install.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# ACTION: Clean system (autoremove not needed packages and remove .deb files)
+# INFO: APT stores a lot of unnecesary files and packages
+# DEFAULT: y
 
+apt-get -y autoremove
+apt-get clean
