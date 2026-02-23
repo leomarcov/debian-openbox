2ff78654b3d6b403570ab56f70e960bce4017698
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-23 12:41:03 +0100

    Update install.sh

--- 00_openbox_install-core/install.sh
+++ 00_openbox_install-core/install.sh
@@ -13,7 +13,8 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 echo -e "\e[1mInstalling packages...\e[0m"
 [ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update
 
-apt-get install -y xserver-xorg xserver-xorg-core xinit xauth x11-common x11-utils x11-xserver-utils x11-xkb-utils xkb-data xterm dbus dbus-x11 dbus-user-session xdg-user-dirs xdg-utils polkitd ukui-polkit
+apt-get install -y xserver-xorg xserver-xorg-core xinit xauth x11-common x11-utils x11-xserver-utils x11-xkb-utils xkb-data xterm dbus dbus-x11 dbus-user-session polkitd ukui-polkit
+apt-get install -y xdg-utils xdg-user-dirs xdg-desktop-portal xdg-desktop-portal-gtk shared-mime-info desktop-file-utils
 apt-get install -y openbox obconf lxappearance compton xfce4-clipman xfce4-power-manager xfce4-settings arandr gsimplecal xcape gparted file-roller xautomation yad inxi
 apt-get install -y network-manager network-manager-applet wpasupplicant wireless-regdb
 
