59659d88b286b2e4bf53432ebcd4977cec36e7d5
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-23 08:29:04 +0100

    Update install.sh

--- 00_config_add-contrib-nonfree/install.sh
+++ 00_config_add-contrib-nonfree/install.sh
@@ -6,6 +6,11 @@
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
+echo 'path-exclude=/usr/share/doc/*
+path-exclude=/usr/share/man/*
+path-exclude=/usr/share/locale/*
+path-include=/usr/share/locale/es*
+path-include=/usr/share/locale/en*' > /etc/dpkg/dpkg.cfg.d/01_nodoc
 
 echo -e "\e[1mAdding APT repositories contrig, non-free and non-free-firmware...\e[0m"
 # Add contrib section
