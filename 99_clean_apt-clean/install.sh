ed62131788d22446ccfc165608291c08401ed740
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-23 14:48:00 +0100

    Update install.sh

--- 99_clean_apt-clean/install.sh
+++ 99_clean_apt-clean/install.sh
@@ -8,8 +8,6 @@
 echo -e "\e[1mUninstalling unnecesary packages...\e[0m"
 apt-get purge -y gnome-keyring modemmanager yelp xdg-desktop-portal-gnome geoclue-2.0 popularity-contest tasksel installation-report
 apt-get purge -y fonts-noto-cjk fonts-noto-extra fonts-noto-ui-extra fonts-noto-unhinted fonts-freefont-ttf 
-apt-get purge -y exim4* mailutils
-apt-get purge -y gnome-desktop3-data gcr gcr4
 apt-get purge -y gvfs*
 apt-get purge -y xfce4-settings xfce4-helpers
 apt-get purge -y brasero-common cdrdao
@@ -18,7 +16,7 @@ apt-get purge -y build-essential gcc g++ make dpkg-dev fakeroot
 apt-get purge -y purge nodejs nodejs-doc node-*
 apt-get purge -y purge sane-utils sane-airscan
  
-# Uninstall unnecesary VirtualBox guest packages
+# Uninstall unnecesary VirtualBox guest packagesevin	
 if [ "$(systemd-detect-virt)" = "oracle" ]; then
 	echo -e "\e[1mUninstalling unnecesary firmware packages for VirtualBox guest...\e[0m"
     apt-get purge -y firmware-realtek firmware-atheros firmware-mediatek firmware-misc-nonfree firmware-amd-graphics
