9d8da442d5d9a42497ce42ec1682b2bcbc6293d3
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-23 08:45:04 +0100

    Update install.sh

--- 00_config_apt-noinstallrecommendes/install.sh
+++ 00_config_apt-noinstallrecommendes/install.sh
@@ -3,4 +3,7 @@
 # INFO: APT install by default recommended packages for each apt install
 # DEFAULT: n
 
+# Check root
+[ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
+
 echo 'APT::Install-Recommends "false";' > /etc/apt/apt.conf.d/99norecommends
