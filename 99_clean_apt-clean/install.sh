47835b76c9db5a5b030134ac4af7b097eb9bafc4
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-24 20:08:21 +0100

    Update install.sh

--- 99_clean_apt-clean/install.sh
+++ 99_clean_apt-clean/install.sh
@@ -6,16 +6,18 @@
 
 # Uninstall unnecesary packages
 echo -e "\e[1mUninstalling unnecesary packages...\e[0m"
-apt-get purge -y gnome-keyring modemmanager yelp xdg-desktop-portal-gnome geoclue-2.0 popularity-contest tasksel installation-report
-apt-get purge -y fonts-noto-cjk fonts-noto-extra fonts-noto-ui-extra fonts-noto-unhinted fonts-freefont-ttf 
-apt-get purge -y gvfs* exim4*
-apt-get purge -y xfce4-settings xfce4-helpers
-apt-get purge -y brasero-common cdrdao
-apt-get purge -y debian-reference-es debian-faq doc-debian
-apt-get purge -y build-essential gcc g++ make dpkg-dev fakeroot
-apt-get purge -y purge nodejs* node-*
-apt-get purge -y purge sane-utils sane-airscan
- 
+pkg_list="${pkg_list} gnome-keyring modemmanager yelp xdg-desktop-portal-gnome geoclue-2.0 popularity-contest tasksel installation-report"
+pkg_list="${pkg_list} fonts-noto-cjk fonts-noto-extra fonts-noto-ui-extra fonts-noto-unhinted fonts-freefont-ttf "
+pkg_list="${pkg_list} gvfs* exim4*"
+pkg_list="${pkg_list} xfce4-settings xfce4-helpers"
+pkg_list="${pkg_list} brasero-common cdrdao"
+pkg_list="${pkg_list} debian-reference-es debian-faq doc-debian"
+pkg_list="${pkg_list} build-essential gcc g++ make dpkg-dev fakeroot"
+pkg_list="${pkg_list} nodejs* node-*"
+pkg_list="${pkg_list} sane-utils sane-airscan libsane*"
+pkg_list=$(set -f; dpkg -l $pkg_list 2>/dev/null | awk '/^ii/{print$2}')	 # Clean pkg_list with only installed packages
+apt-get purge -y ${pkg_list}
+
 # Uninstall unnecesary VirtualBox guest packagesevin	
 if [ "$(systemd-detect-virt)" = "oracle" ]; then
 	echo -e "\e[1mUninstalling unnecesary firmware packages for VirtualBox guest...\e[0m"
