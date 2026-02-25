b09d10aedd6e77212ce0bba9688faad4a9344b88
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-25 09:56:39 +0100

    Update install.sh

--- 00_install_basic-cli-packages/install.sh
+++ 00_install_basic-cli-packages/install.sh
@@ -12,5 +12,5 @@ echo -e "\e[1mInstalling packages...\e[0m"
 [ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update  
 
 apt-get install -y base-files mount procps udev base-passwd bash coreutils dpkg apt systemd systemd-sysv util-linux login passwd tzdata locales tzdata ca-certificates
-apt-get install -y vim less zip unzip rar unrar mtp-tools mailutils traceroute acl gnupg2 plocate apt-transport-https curl wget ntfs-3g file
-apt-get install -y firmware-linux-nonfree
+apt-get install -y vim less zip unzip rar unrar mtp-tools mailutils traceroute acl gnupg2 plocate apt-transport-https curl wget ntfs-3g file p11-kit p11-kit-modules
+
