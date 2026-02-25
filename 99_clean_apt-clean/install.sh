390e4cd5ea31712f2187381a59beb33d159936ee
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-25 11:17:31 +0100

    Update install.sh

--- 99_clean_apt-clean/install.sh
+++ 99_clean_apt-clean/install.sh
@@ -16,12 +16,21 @@ pkg_list="${pkg_list} nodejs* node-*"
 pkg_list="${pkg_list} sane-utils sane-airscan libsane*"
 apt-get purge -y $(set -f; dpkg -l $pkg_list 2>/dev/null | awk '/^ii/{print$2}')
 
-# Uninstall unnecesary VirtualBox guest packages
+# Uninstall unnecesary VirtualBox GUEST packages
 if [ "$(systemd-detect-virt)" = "oracle" ]; then
 	echo -e "\e[1mUninstalling unnecesary firmware packages for VirtualBox guest...\e[0m"
 	pkg_list="firmware-*"
 	apt-get purge -y $(set -f; dpkg -l $pkg_list 2>/dev/null | awk '/^ii/{print$2}')
 fi
+
+# Uninstall old kernels
+echo -e "\e[1mUninstalling old kernels and headers...\e[0m"
+pkg_list="linux-headers* linux-kbuild*"
+for p in $(dpkg -l 'linux-image-[0-9]*' | awk '/^ii/{print $2}'); do
+	v="${p#linux-image-}"
+    dpkg --compare-versions "$v" lt $(uname -r) && pkg_list="${pkg_list} $p"
+done
+apt-get purge -y $(set -f; dpkg -l $pkg_list 2>/dev/null | awk '/^ii/{print$2}')
 
 # APT autoremove and clean
 echo -e "\e[1mCleaning packages and .deb files...\e[0m"
@@ -29,7 +38,7 @@ apt-get -y autoremove --purge
 apt-get clean
 
 echo -e "\e[1m\nAccording your neededs you may be to install:\e[0m
-* gvfs: graphic access to smb, ftp, mtp, trash, automount USB, etc
+* gvfs: graphic access to automount USB, smb, ftp, mtp, trash, etc
 * exim4: mail server
 * avahi: mDNS to detect local hostnames
 * build-essential gcc g++ make dpkg-dev: basic dev tools"
