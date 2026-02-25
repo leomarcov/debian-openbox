50fe2466cc2525f846bf57fa5d4ebfd930a540b2
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2026-02-25 12:16:47 +0100

    Update install.sh

--- 00_openbox_install-core/install.sh
+++ 00_openbox_install-core/install.sh
@@ -10,7 +10,7 @@ base_dir="$(dirname "$(readlink -f "$0")")"
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
 # Install packages
-echo -e "\e[1mInstalling packages...\e[0m"
+echo -e "\n\e[1mInstalling packages...\e[0m"
 [ "$(find /var/cache/apt/pkgcache.bin -mtime 0 2>/dev/null)" ] || apt-get update
 
 apt-get install -y xserver-xorg xserver-xorg-core xserver-xorg-input-libinput xinit xauth x11-common x11-utils x11-xserver-utils x11-xkb-utils xkb-data xterm dbus dbus-x11 dbus-user-session polkitd ukui-polkit
@@ -20,7 +20,32 @@ apt-get install -y libcanberra-gtk3-0 adwaita-icon-theme gtk-update-icon-cache g
 apt-get install -y network-manager network-manager-applet wpasupplicant wireless-regdb
 systemctl disable NetworkManager-wait-online.service
 
-echo -e "\e[1mCopying themes and tools...\e[0m"
+# Installing graphics drivers
+echo -e "\n\e[1mInstalling graphics drivers...\e[0m"
+if systemd-detect-virt -q; then
+    virt=$(systemd-detect-virt)
+    case "$virt" in
+        oracle) 	gpu_pkgs="xserver-xorg-video-vbox"    ;;
+        vmware) 	gpu_pkgs="xserver-xorg-video-vmware" ;;
+        qemu|kvm)   gpu_pkgs="xserver-xorg-video-qxl"    ;;
+        *)          gpu_pkgs="xserver-xorg-video-fbdev"  ;;
+    esac
+else
+	gpu="$(lspci -nn | grep -Ei 'vga|3d|display')"
+    if echo "$gpu" | grep -qi intel; then
+        gpu_pkgs="xserver-xorg-video-intel firmware-misc-nonfree"
+    elif echo "$gpu" | grep -qi amd; then
+        gpu_pkgs="xserver-xorg-video-amdgpu firmware-amd-graphics"
+    elif echo "$gpu" | grep -qi nvidia; then
+        dpkg -l | grep -q '^ii  nvidia-driver'
+		[ $? -eq 0 ] && gpu_pkgs="nvidia-driver firmware-misc-nonfree" || gpu_pkgs="xserver-xorg-video-nouveau firmware-misc-nonfree"            
+    else
+        gpu_pkgs="xserver-xorg-video-fbdev"
+    fi
+fi
+apt-get install -y $gpu_pkgs
+
+echo -e "\n\e[1mCopying themes and tools...\e[0m"
 # Copy theme
 tar -xzvf "$base_dir"/openbox_theme.tgz -C /usr/share/themes/
 cp -rv "$base_dir/openbox-menu" /usr/share/icons/
@@ -47,7 +72,7 @@ chmod a+x /usr/bin/welcome
 cp -v ${base_dir}/welcome.desktop /usr/share/applications/
 
 # Copy users config
-echo -e "\e[1mSetting configs to all users...\e[0m"
+echo -e "\n\e[1mSetting configs to all users...\e[0m"
 for d in /etc/skel /home/*/ /root; do
     [ "$(dirname "$d")" = "/home" ] && ! id "$(basename "$d")" &>/dev/null && continue	# Skip dirs that no are homes
 
@@ -88,5 +113,5 @@ done
 
 
 # Set as default
-echo -e "\e[1mSetting as default alternative...\e[0m"
+echo -e "\n\e[1mSetting as default alternative...\e[0m"
 update-alternatives --set x-session-manager /usr/bin/openbox-session
